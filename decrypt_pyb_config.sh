218091381982c297f5838a7da21fdeda382f32fc
Russ Poldrack <poldrack@gmail.com>
2023-05-31 09:33:32 -0700

    fix output location

--- decrypt_pyb_config.sh
+++ decrypt_pyb_config.sh
@@ -3,6 +3,6 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-mkdir .pybliometrics
+mkdir .config
 gpg --quiet --batch --yes --decrypt --passphrase="$TOKEN_PASSCODE" \
---output .pybliometrics/config.ini config.ini.gpg
+--output .config/pybliometrics.cfg config.ini.gpg
